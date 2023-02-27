78cd7f8a1ea48d40af124a137579a5a8a2eb0454
jamshid <jamshidshamshod0705@gmail.com>
2023-02-27 11:55:50 +0500

    project modal window

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModal = document.querySelector('.close-modal');
+const btns = document.querySelectorAll('.show-modal');
+console.log(btns);
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModalBtn = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener('click', openModal);
+}
+
+closeModal.addEventListener('click', closeModalBtn);
+overlay.addEventListener('click', closeModalBtn);
